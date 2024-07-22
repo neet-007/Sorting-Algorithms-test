ae7cfb5cdd0cfe4ae491ab41df9c256d6c5af7de
neet-007 <moayedeffect@gmail.com>
2024-07-22 23:37:06 +0300

    test 2 is working somewhat well when every thing is in sync classes are applied correctly otherwise it is not

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,31 @@
-import { useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
+import BubbleSort from './SortingComponent/BubbleSort'
+import HeapSort from './SortingComponent/HeapSort'
+import QuickSortBase from './SortingComponent/QuickSortBase'
+import QuickSortDuplicate from './SortingComponent/QuickSortDuplicates'
+import SelectionSort from './SortingComponent/SelectionSort'
 import './App.css'
 
-type BoxType = {
-  val:number;
-  className: 'move-left' | 'move-right' | ''
+export type BoxType = {
+  val:number,
+  className: 'move-right' | 'move-left' | ''
 }
 
+
+
 function App() {
-  const [arr, setArr] = useState<BoxType[]>(Array(7).fill({val:0, className:''}).map(() => ({val:Math.floor(Math.random() * 20), className:''})))
-  const [currindex, setCurrIndex] = useState<number>(-1);
-  const nodeRef = useRef<HTMLDivElement>(null);
-
-  console.log(arr)
-  useEffect(() => {
-    if (currindex < 0){
-      return
-    };
-
-    const timeOut = setTimeout(() => {
-      setArr(prevArr => {
-        const newArr = [...prevArr];
-        const temp = newArr[currindex];
-        newArr[currindex] = newArr[currindex - 1];
-        newArr[currindex - 1] = temp;
-
-        newArr[currindex].className = '';
-        if (currindex > 1){
-          newArr[currindex - 1].className = 'move-right';
-          newArr[currindex - 2].className = 'move-left';
-        }else{
-          newArr[currindex - 1].className = '';
-        };
-
-        return newArr;
-      });
-
-      setCurrIndex(prev => prev === 1 ? -1 : prev - 1)
-    },2000)
-
-    return () => {
-      clearTimeout(timeOut)
-    };
-  },[currindex])
-
-  function startAnimation(){
-    setCurrIndex(arr.length - 1);
-    setArr(prevArr => {
-      const newArr = [...prevArr];
-      newArr[newArr.length - 1].className = 'move-right';
-      newArr[newArr.length - 2].className = 'move-left';
-
-      return newArr
-    });
-  };
-
-  return (
+  const [arr] = useState<BoxType[]>(Array(7).fill({val:0, className:''}).map(() => ({val:Math.floor((Math.random() * (1 - 0.1) + 0.1) * 20), className:''})))
+  const [isSorting, setIsSoring] = useState<boolean>(false);
+  return(
     <div>
-      <div className='container' ref={nodeRef}>
-        {arr.map((num, i) => (
-          <div key={`box-${num.val}-${i}`} className={`box ${num.className}`} style={{height:`${10 * num.val}px`}}>
-            {num.val}
-          </div>
-        ))}
-        </div>
-        <button onClick={startAnimation}>sort</button>
+      <div style={{display:'flex', flexDirection:'column', gap:'1rem'}}>
+        <BubbleSort arr={arr} isSorting={isSorting}/>
+        <SelectionSort arr={arr} isSorting={isSorting}/>
+        <HeapSort arr={arr} isSorting={isSorting}/>
+        <QuickSortBase arr={arr} isSorting={isSorting}/>
+        <QuickSortDuplicate arr={arr} isSorting={isSorting}/>
+      </div>
+      <button onClick={() => setIsSoring(true)}>sort</button>
     </div>
   )
 }
